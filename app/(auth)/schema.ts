c386c73a8b75a73c14cc6a2d1350eaf919802357
yugpokharel <yugpokharel1@gmail.com>
2026-02-09 22:35:32 +0545

    cookie

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -15,10 +15,15 @@ const passwordRule = z
 
 export const registerSchema = z
     .object({
-        name: z.string().min(2, "Name is too short"),
+        fullName: z.string().min(2, "Full name is too short"),
+        username: z.string().min(2, "Username is too short"),
+        phoneNumber: z.string().min(7, "Phone number is too short"),
         email: z.string().email("Please provide valid email address"),
         password: passwordRule,
         confirmPassword: passwordRule,
+        profilePicture: z.instanceof(File, {
+            message: "Please select a profile picture",
+        }),
     })
     .superRefine((data, ctx) => {
         if (data.password !== data.confirmPassword) {
